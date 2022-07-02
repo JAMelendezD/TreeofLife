fe10fdd238fb38a3ba9d28f1b7c702717cd53cf0
JAMelendezD <ja.melendez10@uniandes.edu.co>
2022-07-02 09:31:10 -0400

    new attempt large data base

--- scrape_wiki.py
+++ scrape_wiki.py
@@ -5,8 +5,7 @@ import numpy as np
 import multiprocessing as mp
 from multiprocessing.pool import Pool
 from tqdm import tqdm
-
-
+import os
 
 node_names = pd.read_table('./ott/taxonomy.tsv', 
         usecols = [4]).to_numpy()
@@ -37,38 +36,42 @@ r  = "\x1b[0m"
 
 f = open('info.csv', 'w')
 
-def scrape(start, stop):
-
-    for i in range(start,stop):
-        data = node_names[i][0].split()[0:2]
-        size = len(data)
-
-        if size == 1 or size == 2:
-            if size == 1:
-                webpage = f"https://en.wikipedia.org/wiki/{data[0].capitalize()}"
-            else:
-                webpage = f"https://en.wikipedia.org/wiki/{data[0].capitalize()}_{data[1]}"
+def scrape(name):
 
 
-            try:
-                page = requests.get(webpage)
-            except:
-                print(30*"#"+"\n"+f"Failed at index: {i}")
+    webpage = f"https://en.wikipedia.org/wiki/{name.capitalize()}"
+
+    try:
+        page = requests.get(webpage)
+    except:
+        print(30*"#"+"\n"+f"Failed at index: {i}")
+    
+    if page.status_code != 404:
+        soup = BeautifulSoup(page.content, 'html.parser')
+        for paragraph in soup.find_all('p'):
+            text = str(paragraph.get_text())
+            if len(text) >= 100:
+                print(Y, name, G, text[0:200].replace('\n', ' '), "...", r)
+                line = f"{name}," + "\"" + text[0:200] + "..."
+                f.write(line.replace('\n', '') + "\"\n")
                 break
-            
-            if page.status_code != 404:
-                soup = BeautifulSoup(page.content, 'html.parser')
-                for paragraph in soup.find_all('p'):
-                    text = str(paragraph.get_text())
-                    if len(text) >= 100:
-                        print(Y, name_num[node_names[i][0]], node_names[i][0], G, text[0:200].replace('\n', ' '), "...", r)
-                        line = f"{name_num[node_names[i][0]]}," + "\"" + text[0:200] + "..."
-                        f.write(line.replace('\n', '') + "\"\n")
-                        break
-            else:
-                print(Y, name_num[node_names[i][0]], node_names[i][0], R, "No information found", r)
-                line = f"{name_num[node_names[i][0]]}," + "\"No information found"
-                #f.write(line + "\"\n")
+    else:
+        print(Y, name, R, "No information found", r)
+        line = f"{name}," + "\"" + "No information found"
+        #f.write(line + "\"\n")
 
 if __name__ == "__main__":
-    scrape(0, len(node_names)+1)
+    
+    tmp = []
+    for i in tqdm(range(len(node_names))):
+        data = node_names[i][0].split()[0:1]
+        tmp.append(data[0])
+
+    unique = np.unique(tmp)
+
+    abcd = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    out = 256
+    for dat in unique:
+        if dat[0] in abcd:
+            scrape(dat)
